ec274f028364b67d969a9ae2c444da28e343a3de
Padilla <francisco.j.padilla@accenture.com>
2020-10-31 22:00:28 -0600

    remove dotenv

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const app = require('./routes');
 const mongoose = require('mongoose');
 const port = process.env.PORT || 3977
-require('dotenv').config();
 
 mongoose.set('useFindAndModify', false);
 
